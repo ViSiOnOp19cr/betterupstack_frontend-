27c478b7485c735b43bf548102642a4a407bd7c0
ViSiOnOp19cr <chandancr98195@gmail.com>
2025-08-28 14:57:30 +0530

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['upgaurd.chandancr.xyz'], 
+    port: 5173                              
+  },
+  server: {
+    host: true 
+  }
 })
